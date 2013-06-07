ef6370c933dce2b273a0151d69c35b70aa6127a8
Paul Eagle <pauleagle@gmail.com>
2013-06-07 09:51:55 -0700

    Update cvsWizardEyes.sh
    Added CVS Update. Added functions for dialog boxs to clean up code.

--- cvsWizardEyes.sh
+++ cvsWizardEyes.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#  The MIT License (MIT)
+#  Copyright (c) 2013 microuser pauleagle@gmail.com
+#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 #////////////////////////////
 #Server/Remote Configuration
 cvsServerHostname="cvs";
@@ -9,17 +16,48 @@ cvsServerMethod="pserver"
 #Client/Local Configuration
 userName=`whoami`
 cvsLocalBase=~/cvs;
-
+venderTag="VenderName";
+releaseTag="R0_1";
 #Project Configuration
 projectName="php/"
 #CVS Commands
 commandDirectory="-d"
+commandComment="-m"
 #////////////////////////////
 
 setProjectName (){
 #vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 exec 3>&1
 projectName=$(dialog --inputbox "Project/Module Name in $cvsServerBase" 10 70 "$projectName" 2>&1 1>&3)
+exec 3>&-
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+}
+
+setHostname () {
+#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+exec 3>&1
+cvsServerHostname=$(dialog --inputbox "Enter the cvs hostname" 10 70 $cvsServerHostname 2>&1 1>&3)
+exec 3>&-
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+}
+setCvsServerBase () {
+#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+exec 3>&1
+cvsServerBase=$(dialog --inputbox "Enter the server's repository path" 10 70 $cvsServerBase 2>&1 1>&3)
+exec 3>&-
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+}
+setUserName(){
+#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+exec 3>&1
+userName=$(dialog --inputbox "Enter your CVS username" 10 70 $userName 2>&1 1>&3)
+exec 3>&-
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+}
+setUserComment(){
+#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+exec 3>&1
+userComment=$(dialog --inputbox "Enter a comment" 10 70 "$(date)" 2>&1 1>&3)
 exec 3>&-
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 }
@@ -47,23 +85,9 @@ echo .
 echo .
 echo "CVS Manager with Wizzard Eyes";
 
-#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-exec 3>&1
-cvsServerHostname=$(dialog --inputbox "Enter the cvs hostname" 10 70 $cvsServerHostname 2>&1 1>&3)
-exec 3>&-
-#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-exec 3>&1
-cvsServerBase=$(dialog --inputbox "Enter the server's repository path" 10 70 $cvsServerBase 2>&1 1>&3)
-exec 3>&-
-#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-exec 3>&1
-userName=$(dialog --inputbox "Enter your CVS username" 10 70 $userName 2>&1 1>&3)
-exec 3>&-
-#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+setHostname
+setCvsServerBase
+setUserName
 
 export CVSROOT=:$cvsServerMethod:$userName@$cvsServerHostname:$cvsServerBase
 
@@ -87,22 +111,18 @@ exec 3>&-
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 ################################################################################
-if [ "$userMenu" = "New" ] # strings are equal #################################
+if [ "$userMenu" = "Import" ] # strings are equal #################################
 ################################################################################
 then
-	echo "Creating new project";
-	#dialog --form "Create a New Project" 30 60 6 \
-	#"label" 1 1 "User" 1 10 10 0 \
-	#"label" 2 1 "labl2" 2 10 15 0 \
-	
+	setProjectName
+	setUserComment
+		
 	#Make the directory if not exist
 	mkdir -p $cvsLocalBase/$projectName
-
-
 	
 	#Set current directory for CVS
 	cd $cvsLocalBase/$projectName
-	
+
 	#Print the items of current directory
 	ls
 	
@@ -110,9 +130,13 @@ then
 	releaseTag="RELEASE_TAG"
 	#Create Project
 	export CVSROOT=:$cvsServerMethod:$userName@$cvsServerHostname:$cvsServerBase
-	echo Using CVSROOT=$CVSROOT
+	echo Using Remote CVSROOT=$CVSROOT
+	echo Using Local $cvsLocalBase/$projectName
+
 	tree -d
-	#cvs import $commandDirectory $cvsServerBase
+
+	cvs import $commandComment \""$userComment"\" \""$projectName"\" $venderTag $releaseTag
+
 
 
 
