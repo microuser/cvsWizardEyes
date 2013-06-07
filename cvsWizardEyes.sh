b20a3fe23e7139372fbd4c6de295e70b817c8b8a
microuser <pauleagle@gmail.com>
2013-06-06 17:33:48 -0700

    Update cvsWizardEyes.sh

--- cvsWizardEyes.sh
+++ cvsWizardEyes.sh
@@ -2,19 +2,27 @@
 
 #////////////////////////////
 #Server/Remote Configuration
-cvsServerHostname="cvs1.gvea.local";
+cvsServerHostname="cvs";
 cvsServerBase="/var/cvsroot/applications/"
 cvsServerMethod="pserver"
 
 #Client/Local Configuration
-userName="<YOUR USER NAME>"
-cvsLocalBase="~/cvs";
+userName=`whoami`
+cvsLocalBase=~/cvs;
 
 #Project Configuration
-projectName="cvsWizzrdEyesProject"
+projectName="php/"
 #CVS Commands
 commandDirectory="-d"
 #////////////////////////////
+
+setProjectName (){
+#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+exec 3>&1
+projectName=$(dialog --inputbox "Project/Module Name in $cvsServerBase" 10 70 "$projectName" 2>&1 1>&3)
+exec 3>&-
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+}
 
 checkDependenciesAndInstall () {
 #if cvs is installed, try cvs, redirect errors to stdout and count the lines matching notfound.
@@ -38,6 +46,25 @@ echo .
 echo .
 echo .
 echo "CVS Manager with Wizzard Eyes";
+
+#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+exec 3>&1
+cvsServerHostname=$(dialog --inputbox "Enter the cvs hostname" 10 70 $cvsServerHostname 2>&1 1>&3)
+exec 3>&-
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+exec 3>&1
+cvsServerBase=$(dialog --inputbox "Enter the server's repository path" 10 70 $cvsServerBase 2>&1 1>&3)
+exec 3>&-
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+exec 3>&1
+userName=$(dialog --inputbox "Enter your CVS username" 10 70 $userName 2>&1 1>&3)
+exec 3>&-
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 export CVSROOT=:$cvsServerMethod:$userName@$cvsServerHostname:$cvsServerBase
 
 #exec 3>&1
@@ -45,18 +72,17 @@ export CVSROOT=:$cvsServerMethod:$userName@$cvsServerHostname:$cvsServerBase
 #exec 3>&-
 cvs login
 
-dialog --msgbox "CVS Wizzard Eyes\n Instructions:" 20 40 
+dialog --msgbox "CVS Wizzard Eyes\n Instructions:" 10 70 
 
 
 #vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 exec 3>&1
-$cvsServerHostname=$(dialog --inputbox "Enter the cvs hostname" 20 30 $cvsServerHostname 2>&1 1>&3)
-exec 3>&-
-#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-#vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-exec 3>&1
-userMenu=$(dialog --menu "CVS Wizzard Eyes Main Menu" 30 60 4 "Import" "Create a new project from directory" "Checkout" "Download Source into directory"  "Update" "Download source updates (distructive)." "Checkin" "Upload Source from directory" 2>&1 1>&3)
+userMenu=$(dialog --menu "CVS Wizzard Eyes Main Menu" 15 70 4 \
+		"Import"	"Create a new project from directory" \
+		"Checkout"	"Download Source into directory" \
+		"Update"	"Download source updates (distructive)." \
+		"Checkin"	"Upload Source from directory" \
+	2>&1 1>&3)
 exec 3>&-
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
@@ -64,13 +90,15 @@ exec 3>&-
 if [ "$userMenu" = "New" ] # strings are equal #################################
 ################################################################################
 then
-  echo "Creating new project";
+	echo "Creating new project";
 	#dialog --form "Create a New Project" 30 60 6 \
 	#"label" 1 1 "User" 1 10 10 0 \
 	#"label" 2 1 "labl2" 2 10 15 0 \
 	
 	#Make the directory if not exist
 	mkdir -p $cvsLocalBase/$projectName
+
+
 	
 	#Set current directory for CVS
 	cd $cvsLocalBase/$projectName
@@ -91,20 +119,18 @@ then
 ################################################################################
 elif [ "$userMenu" = "Checkout" ] ##############################################
 ################################################################################
-
-exec 3>&1
-projectName=$(dialog --inputbox "Module Name in $cvsServerBase" 30 40 2>&1 1>&3)
-exec 3>&-
-cd $cvsLocalBase/$projectName
-rm -r $cvsLocalBase/$projectName
+then
+dialog --msgbox "About to checkout" 20 30
+setProjectName
+mkdir -p $cvsLocalBase
+cd $cvsLocalBase
 cvs checkout $projectName
 echo ---------------------------------------------------------------------------
 echo ===========================================================================
 echo ---------------------------------------------------------------------------
 echo `pwd`
 tree -d | grep -v "── CVS"
-then
-echo "Checking out project";
+echo "Project @ $cvsServerBase$projectName should now be checked out.";
 ################################################################################
 elif [ "$userMenu" = "Update" ] ################################################
 ################################################################################
@@ -114,7 +140,16 @@ echo "Updating project";
 elif [ "$userMenu" = "Checkin" ] ###############################################
 ################################################################################
 then
-echo "Checking in project";
+dialog --msgbox "About to checkin" 20 30
+setProjectName
+cd $cvsLocalBase/$projectName
+cvs commit $projectName
+echo ---------------------------------------------------------------------------
+echo ===========================================================================
+echo ---------------------------------------------------------------------------
+echo `pwd`
+tree -d | grep -v "── CVS"
+echo "Project @ $cvsServerBase$projectName should now be checked in.";
 fi
 
 
